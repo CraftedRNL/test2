48f2341224a3673b1b689156b6af10ef637fc502
CraftedRNL <126Rliu@frhsd.com>
2024-10-07 13:51:19 -0400

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+// Basic Variables and Data Types
+
+// 1.   Declare a variable named number and assign the value 100 to it.
+var number = 100;
+// 2.   Add 10 to the value of number and store the result back in number.
+number = number +10;
+// 3.   Add another 10 to the value of number using the += operator.
+number +=10;
+// 4.   Log the current value of number to the console.
+console.log(number);
+// 5.   Declare a variable named age and assign the value 7 to it.
+var age = 7;
+// 6.   Create a string that says "Tommy is" followed by the value of age and "years old."
+var string = "Tommy is "+ age+" years old."; 
+// 7.   Create the same string using template literals.
+var string2 = `Tommy is ${age} years old.`;
+// 8.   Declare a constant named currency and assign the value "$" to it.
+const currency = "$";
+// 9.   Declare a variable named userIncome and assign the value 85000 to it.
+var userIncome = 85000;
+// 10.  Create a string that says "Your income is" followed by the currency and userIncome, and log it to the console.
+var string3 = "Your income is " + currency + userIncome + ".";
+console.log(string3);
